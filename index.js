7c21cda3ceb1de716ad887bae49ff09bf988e337
Henri Bergius <henri.bergius@iki.fi>
2014-02-20 11:09:42 +0100

    CoffeeScript 1.7 compatibility, fixes #2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('coffee-script');
+var CoffeeScript = require('coffee-script');
+CoffeeScript.register();
 exports.Point = require('./src/Point.coffee').Point;
 exports.BBox = require('./src/BBox.coffee').BBox;
 exports.Geocoder = require('./src/Geocoder.coffee').Geocoder;
